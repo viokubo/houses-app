055e5e7ebc88024b56201b1eda4879fdf9db9915
Vitor Okubo <vhideki1996@icloud.com>
2024-12-14 20:46:27 -0300

    add router config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    HomeComponent
+    HomeComponent,
+    RouterModule
   ],
   template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <app-home></app-home>
-    </section>
-  </main>
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
   `,
   styleUrl: './app.component.css'
 })
